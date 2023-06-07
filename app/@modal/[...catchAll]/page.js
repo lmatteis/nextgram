bab5b836b745bd7583236e3c2ce15028c5d6412b
Luca Matteis <lmatteis@gmail.com>
2023-06-07 11:34:03 +0200

    Add catchall

---
+++ app/@modal/[...catchAll]/page.js
@@ -0,0 +1,4 @@
+export default function CatchAll() {
+  console.log("catching");
+  return null;
+}
--- components/frame/index.js
+++ components/frame/index.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Photo({ photo }) {
   return (
@@ -12,6 +13,12 @@ export default function Photo({ photo }) {
       />
 
       <div className="bg-white p-4 px-6">
+        <h3>
+          <Link href="/photos/9">click me</Link>
+        </h3>
+        <h3>
+          <Link href="/">close</Link>
+        </h3>
         <h3>{photo.name}</h3>
         <p>Taken by {photo.username}</p>
       </div>
